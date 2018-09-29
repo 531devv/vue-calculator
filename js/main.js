b31c66de642b53371af49ac6cdbec2f6bf5d56fb
Marek <mail@531devv.pl>
2018-09-29 21:53:50 +0200

    Put js to another file

---
+++ js/main.js
@@ -0,0 +1,56 @@
+var app = new Vue({
+    el: '#calculator',
+    data: {
+        previous: null,
+        current: '',
+        operator: null,
+        operatorClicked: false
+    },
+    methods: {
+        percent() {
+            this.current = parseFloat(this.current / 100);
+        },
+        clear() {
+            this.current = ''
+        },
+        append(number) {
+            if(this.operatorClicked){
+                this.current = '';
+                this.operatorClicked = false;
+            }
+            if(this.current === '0'){
+                this.current = '';
+            }
+            this.current = this.current + number;
+        },
+        setPrevious(){
+            this.previous = this.current;
+            this.operatorClicked = true;
+        },
+        divide() {
+            this.operator = (a, b) => a / b;
+            this.setPrevious();
+        },
+        times() {
+            this.operator = (a, b) => a * b;
+            this.setPrevious();
+        },
+        minus() {
+            this.operator = (a, b) => a - b;
+            this.setPrevious();
+        },
+        plus() {
+            this.operator = (a, b) => a + b;
+            this.setPrevious();
+        },
+        dot() {
+            if(this.current.indexOf('.') === '-1'){
+                this.append('.');
+            }
+        },
+        equal() {
+            this.current = this.operator(parseFloat(this.current), parseFloat(this.previous))
+            this.previous = null;
+        }
+    }
+})
